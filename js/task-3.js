f38c97d774a2427709799b16ba1d9f2cb3093c1e
Oleksandr <143627784+Sandro-it@users.noreply.github.com>
2023-11-17 22:27:24 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+"use strict";
+function getElementWidth(content, padding, border) {
+  // Перетворюємо значення параметрів у числовий формат
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding);
+  const borderWidth = parseFloat(border);
+
+  // Обчислюємо загальну ширину елемента, враховуючи box-sizing: border-box
+  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+
+  return totalWidth;
+}
+
+// Перевірка роботи функції
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
